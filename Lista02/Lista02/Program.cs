e68f00d7ea55d237af4774560dbd29aac736a373
Diogo Souza <diogouba11@gmail.com>
2023-02-27 19:20:59 -0300

    correcoes

--- Lista02/Lista02/Program.cs
+++ Lista02/Lista02/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Lista02
 {
-    class Ex01
+    internal class Program
     {
-        static void Main(string[] args)
+        static void Ex01(string[] args)
         {
             int[] valores = new int[172];
             int menor = int.MaxValue;
@@ -47,11 +47,8 @@ namespace Lista02
             Console.WriteLine("Maior valor encontrado: " + maior);
             Console.WriteLine("Média aritmética dos valores pares e menores que 100: " + mediaParesMenoresCem);
         }
-    }
 
-    class Ex02
-    {
-        static void Main(string[] args)
+        static void Ex02(string[] args)
         {
             int quantidade = 0;
             int soma = 0;
@@ -79,10 +76,8 @@ namespace Lista02
 
             Console.WriteLine("A média dos números digitados é: " + media);
         }
-    }
-    class Ex03
-    {
-        static void Main(string[] args)
+
+        static void Ex03(string[] args)
         {
             Console.WriteLine("Digite os valores de X e Y (separados por espaço):");
             string[] valores = Console.ReadLine().Split(' ');
@@ -100,10 +95,8 @@ namespace Lista02
                 }
             }
         }
-    }
-    class Ex04
-    {
-        static void Main(string[] args)
+
+        static void Ex04(string[] args)
         {
             Console.WriteLine("Digite um número positivo:");
             int p = int.Parse(Console.ReadLine());
@@ -122,10 +115,8 @@ namespace Lista02
                 }
             }
         }
-    }
-    class Ex05
-    {
-        static void Main(string[] args)
+
+        static void Ex05(string[] args)
         {
             Console.WriteLine("Digite o número de linhas:");
             int n = int.Parse(Console.ReadLine());
@@ -142,10 +133,8 @@ namespace Lista02
                 Console.WriteLine();
             }
         }
-    }
-    class Ex06
-    {
-        static void Main(string[] args)
+
+        static void Ex06(string[] args)
         {
             Console.WriteLine("Digite o número de linhas:");
             int n = int.Parse(Console.ReadLine());
@@ -171,56 +160,54 @@ namespace Lista02
                 caracteres += 2;
             }
         }
-    }
-    class Menu
-    {
-        static void main(string[] args)
+
+        static void Menu(string[] args)
         {
             static void Main(string[] args)
             {
-            Console.WriteLine("Escolha uma opção:");
-            Console.WriteLine("1 - Exercício 1");
-            Console.WriteLine("2 - Exercício 2");
-            Console.WriteLine("3 - Exercício 3");
-            Console.WriteLine("4 - Exercício 4");
-            Console.WriteLine("5 - Exercício 5");
-            Console.WriteLine("6 - Exercício 6");
-            Console.WriteLine("7 - Sair");
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("1 - Exercício 1");
+                Console.WriteLine("2 - Exercício 2");
+                Console.WriteLine("3 - Exercício 3");
+                Console.WriteLine("4 - Exercício 4");
+                Console.WriteLine("5 - Exercício 5");
+                Console.WriteLine("6 - Exercício 6");
+                Console.WriteLine("7 - Sair");
 
-            int opcao = int.Parse(Console.ReadLine());
+                int opcao = int.Parse(Console.ReadLine());
 
-            switch (opcao)
+                switch (opcao)
                 {
-                case 1:
-                    Ex01.Main(args);
-                    break;
-                case 2:
-                    Ex02.Main(args);
-                    break;
-                case 3:
-                    Ex03.Main(args);
-                    break;
-                case 4:
-                    Ex04.Main(args);
-                    break;
-                case 5:
-                    Ex05.Main(args);
-                    break;
-                case 6:
-                    Ex06.Main(args);
-                    break;
-                case 7:
-                    return;
-                default:
-                    Console.WriteLine("Opção inválida!");
-                    break;
-            }
+                    case 1:
+                        Ex01.Main(args);
+                        break;
+                    case 2:
+                        Ex02.Main(args);
+                        break;
+                    case 3:
+                        Ex03.Main(args);
+                        break;
+                    case 4:
+                        Ex04.Main(args);
+                        break;
+                    case 5:
+                        Ex05.Main(args);
+                        break;
+                    case 6:
+                        Ex06.Main(args);
+                        break;
+                    case 7:
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
 
-            Console.WriteLine("Pressione qualquer tecla para continuar...");
-            Console.ReadKey();
-            Console.Clear();
-            Main(args);
-        }   
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+                Main(args);
+            }
         }
     }
 }
